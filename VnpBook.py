ae7721391a5f3f2a1b958de3d75e94b0fc3b27ba
Remy Berg <razielbe@gmail.com>
2015-12-08 13:16:16 +0200

    First commit, script basicly works

---
+++ VnpBook.py
@@ -0,0 +1,68 @@
+﻿#!/usr/bin/python
+# Filename: VnpBook.py
+
+import urllib2
+
+
+class VpnBookHandler(object):
+    """Access vpn book and grab the password"""
+
+    __startString = "<li>Password: <strong>"
+    __endString = "</strong></li>"
+    version = '0.1'
+
+    def getVpnPassword(self):
+        """Do some things.
+        """
+        #content = urllib2.urlopen("https://www.vpnbook.com/freevpn").read()
+        #password = self.CutPassword(content)
+
+        r = urllib2.Request("https://www.vpnbook.com/freevpn");
+        f = urllib2.build_opener().open(r)
+        response = f.read()
+        s = self.__CutPassword(response)
+
+        return (s)
+
+
+    def __CutPassword(self, httpContent):
+        """Cut the password.
+        :param httpContent: VpnHandler.
+        """
+        
+        startIndex = httpContent.find(self.__startString)
+        stopIndex = httpContent.find(self.__endString, startIndex)
+        password = httpContent[startIndex + len(self.__startString):stopIndex]
+        return (password)
+
+class WindowsVpnHandler(object):
+    POWERSHELL = "powershell "
+
+    def StartVpn(self, vpnName, ServerAddress, username, password, tunnelType = "Pptp"):
+        if (self._IsVpnExist(vpnName) == False):
+            print(self._CreateVpn(vpnName, ServerAddress, tunnelType))
+        print(self._Connect(vpnName, username, password))
+
+    def _IsVpnExist(self, vpnName):
+        return False
+
+    def _Connect(self, vpnName, userName, password):
+        return subprocess.call(self.POWERSHELL + "rasdial {0} {1} {2}".format(vpnName, userName, password))
+
+    def _CreateVpn(self, vpnName, ServerAddress, TunnelType):
+        return  subprocess.call(self.POWERSHELL +
+                                "Add-VpnConnection -Name \"{0}\" -ServerAddress \"{1}\" -TunnelType \"{2}\""
+                                .format(vpnName, ServerAddress, TunnelType))
+
+if (__name__ == '__main__'):
+    c = VpnBookHandler()
+    password = c.getVpnPassword()
+    print(password)
+    import platform
+    is_windows = any(platform.win32_ver())
+    if (is_windows == True):
+        import subprocess
+        windowsHandler = WindowsVpnHandler()
+        windowsHandler.StartVpn("'Canadian Vpn'","ca1.vpnbook.com", "vpnbook", password,)
+
+# End of VnpBook.py
---
+++ readme.md
@@ -0,0 +1,12 @@
+<h1> HALLO!</h1>
+
+I created this project since i saw there is no windows auto connector for the lovley vpnbook.
+
+Basicly this script is ready for use, just launch it and it will setup and connect to the canadian vpn.
+
+This is simple 50 lines script.
+
+
+ToDo: 
+
+Create "_IsVpnExist" function.
